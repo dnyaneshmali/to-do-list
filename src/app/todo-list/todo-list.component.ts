0c8de7f74f34d40a3670766c340cae14f100a380
dnyaneshmali <sanket7575757575@gmail.com>
2024-06-19 00:48:34 +0530

    format documents

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -9,39 +9,40 @@ import { CommonModule } from '@angular/common';
   styleUrl: './todo-list.component.css',
   imports: [CommonModule],
   standalone: true,
-  host: {ngSkipHydration: 'true'}
+  host: { ngSkipHydration: 'true' },
 })
 export class TodoListComponent implements OnInit {
-    todoListData: TodoListModel[] = [];
-    userTodoList: { [key: number]: TodoListModel[] } = {};
-  
-    constructor(private todoListService: TodoListService) { }
-  
-    ngOnInit(): void {
-      this.getTodoList();
-    }
+  todoListData: TodoListModel[] = [];
+  userTodoList: { [key: number]: TodoListModel[] } = {};
+  alertMessage: string = '';
 
-    getTodoList() {
-        this.todoListService.getTodoList().subscribe(todoListData => {
-            if(todoListData.length) {
-                this.todoListData = todoListData;
-                this.getTasksByUser();
-            } else {
-                
-            }
-        });
-    }
-  
-    getTasksByUser(): void {
-      this.todoListData.forEach(todoItem => {
-        if (!this.userTodoList[todoItem.userId]) {
-          this.userTodoList[todoItem.userId] = [];
-        }
-        this.userTodoList[todoItem.userId].push(todoItem);
-      });
-    }
+  constructor(private todoListService: TodoListService) {}
 
-    getTodoListUserIds(): number[] {
-        return Object.keys(this.userTodoList).map(Number);
+  ngOnInit(): void {
+    this.getTodoList();
+  }
+
+  getTodoList() {
+    this.todoListService.getTodoList().subscribe((todoListData) => {
+      if (todoListData.length) {
+        this.todoListData = todoListData;
+        this.getTasksByUser();
+      } else {
+        this.alertMessage = 'To Do List is not available, please try again !!';
       }
+    });
+  }
+
+  getTasksByUser(): void {
+    this.todoListData.forEach((todoItem) => {
+      if (!this.userTodoList[todoItem.userId]) {
+        this.userTodoList[todoItem.userId] = [];
+      }
+      this.userTodoList[todoItem.userId].push(todoItem);
+    });
+  }
+
+  getTodoListUserIds(): number[] {
+    return Object.keys(this.userTodoList).map(Number);
+  }
 }
--- src/app/todo-list/todo-list.service.ts
+++ src/app/todo-list/todo-list.service.ts
@@ -4,15 +4,14 @@ import { TodoListModel } from './todo-list-model';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoListService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
-    private apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient) { }
-  
-    getTodoList(): Observable<TodoListModel[]> {
-      return this.http.get<TodoListModel[]>(this.apiUrl);
-    }
+  getTodoList(): Observable<TodoListModel[]> {
+    return this.http.get<TodoListModel[]>(this.apiUrl);
+  }
 }
